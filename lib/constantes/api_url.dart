bc2f887278f9b1deb3c3bd15c3f60ec58da20968
alerrandro borges <alerrandrokaton@gmail.com>
2023-11-07 16:34:59 -0300

    feat: constants

--- lib/constantes/api_url.dart
+++ lib/constantes/api_url.dart
@@ -1,3 +1,4 @@
 class ApiContants {
-  static String baseApi = "http://10.0.0.149:3000/api/v1";
+  static String baseApi = "https://api.velocitynet.com.br/api/v1";
+  // static String baseApi = "http://10.0.0.149:3000/api/v1";
 }
